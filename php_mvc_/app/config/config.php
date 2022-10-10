0e37e7e6d5a7985d0a26ecb49edf7f65b27c2050
Heryani00 <heryanishyt1105@gmail.com>
2022-10-11 00:11:38 +0700

    menyelesaikan video 8

---
+++ php_mvc_/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_c_203040169/php_mvc_/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'php_mvc_');
--- php_mvc_/app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/prakweb_c_203040169/php_mvc_/public');
